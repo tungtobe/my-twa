48cfb4a09e741d4b781b2e1c6e275d4514a46e61
tungtobe <tungtobe@gmail.com>
2024-03-23 16:59:00 +0900

    Refactor App component and fix increment button placement

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
-import { useCounterContract } from "./hooks/useCounterContract";
 import { useTonConnect } from "./hooks/useTonConnect";
+import { useCounterContract } from "./hooks/useCounterContract";
 import "@twa-dev/sdk";
 
 function App() {
   const { connected } = useTonConnect();
   const { value, address, sendIncrement } = useCounterContract();
+
   return (
     <div className="App">
       <div className="Container">
@@ -21,16 +22,16 @@ function App() {
           <b>Counter Value</b>
           <div>{value ?? "Loading..."}</div>
         </div>
-      </div>
 
-      <a
-        className={`Button ${connected ? "Active" : "Disabled"}`}
-        onClick={() => {
-          sendIncrement();
-        }}
-      >
-        Increment
-      </a>
+        <a
+          className={`Button ${connected ? "Active" : "Disabled"}`}
+          onClick={() => {
+            sendIncrement();
+          }}
+        >
+          Increment
+        </a>
+      </div>
     </div>
   );
 }
