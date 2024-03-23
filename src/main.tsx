2272dc859f98dd473a9088f23e7243d5234d840a
tungtobe <tungtobe@gmail.com>
2024-03-23 16:10:52 +0900

    Update URLs and manifest configuration

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  "https://tungtobe.github.io/my-twa/tonconnect-manifest.json";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
