dbc578585b11076cf2cb401a99bcedfa04bf0a4c
tungtobe <tungtobe@gmail.com>
2024-03-28 00:34:08 +0900

    Add transferTon function to onClick event in Send 1 TON button

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useCounterContract } from "./hooks/useCounterContract";
 import "@twa-dev/sdk";
 
 function App() {
-  const { connected } = useTonConnect();
+  const { connected, transferTon } = useTonConnect();
   const { value, address, sendIncrement } = useCounterContract();
   return (
     <div className="App">
@@ -30,7 +30,7 @@ function App() {
         </a>
         <a
           className={`Button ${connected ? "Active" : "Disabled"}`}
-          href={`ton://transfer/UQB6ahxEuPUmT9w8VDxcXY0Ftw6kT6daXDPiZNo6rLyklG1I?amount=1000000000&text=billID1111`}
+          onClick={() => transferTon("1", "billID1111")}
         >
           Send 1 TON
         </a>
