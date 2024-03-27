09e1bc788e03b208759b5adee0ddaa73f54b3b62
tungtobe <tungtobe@gmail.com>
2024-03-28 00:14:30 +0900

    Add "Send 1 TON" button to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,24 @@ import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { useCounterContract } from "./hooks/useCounterContract";
+
 import "@twa-dev/sdk";
 
 function App() {
-  const { connected } = useTonConnect();
+  const { connected, transferTon } = useTonConnect();
   const { value, address, sendIncrement } = useCounterContract();
-
   return (
     <div className="App">
       <div className="Container">
         <TonConnectButton />
-
         <div className="Card">
           <b>Counter Address</b>
           <div className="Hint">{address?.slice(0, 30) + "..."}</div>
         </div>
-
         <div className="Card">
           <b>Counter Value</b>
           <div>{value ?? "Loading..."}</div>
         </div>
-
         <a
           className={`Button ${connected ? "Active" : "Disabled"}`}
           onClick={() => {
@@ -30,6 +27,12 @@ function App() {
           }}
         >
           Increment
+        </a>
+        <a
+          className={`Button ${connected ? "Active" : "Disabled"}`}
+          href={`ton://transfer/UQB6ahxEuPUmT9w8VDxcXY0Ftw6kT6daXDPiZNo6rLyklG1I?amount=1000000000&text=billID1111`}
+        >
+          Send 1 TON
         </a>
       </div>
     </div>
--- src/hooks/useTonConnect.ts
+++ src/hooks/useTonConnect.ts
@@ -1,7 +1,13 @@
 import { useTonConnectUI } from "@tonconnect/ui-react";
 import { Sender, SenderArguments } from "@ton/core";
+import { Address, toNano } from "@ton/core";
+import { beginCell } from "@ton/ton";
 
-export function useTonConnect(): { sender: Sender; connected: boolean } {
+export function useTonConnect(): {
+  sender: Sender;
+  connected: boolean;
+  transferTon: (ton_amount: string, text: string) => void;
+} {
   const [tonConnectUI] = useTonConnectUI();
 
   return {
@@ -20,5 +26,23 @@ export function useTonConnect(): { sender: Sender; connected: boolean } {
       },
     },
     connected: tonConnectUI.connected,
+    transferTon: async (ton_amount: string, text: string) => {
+      const body = beginCell()
+        .storeUint(0, 32) // write 32 zero bits to indicate that a text comment will follow
+        .storeStringTail(text) // write our text comment
+        .endCell();
+      tonConnectUI.sendTransaction({
+        validUntil: Math.floor(Date.now() / 1000) + 360,
+        messages: [
+          {
+            address: Address.parse(
+              "UQB6ahxEuPUmT9w8VDxcXY0Ftw6kT6daXDPiZNo6rLyklG1I"
+            ).toRawString(),
+            amount: toNano(ton_amount).toString(),
+            payload: body.toBoc().toString("base64"), // payload with comment in body
+          },
+        ],
+      });
+    },
   };
 }
